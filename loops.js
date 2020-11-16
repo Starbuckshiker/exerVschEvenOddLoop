e4e49b0f275ab611ba8481042fcfb1876ec317b0
starbuckshiker <u1304954@umail.utah.edu>
2020-11-16 14:20:28 -0700

    break

--- loops.js
+++ loops.js
@@ -1,8 +1,12 @@
 // Javascript Loops
 
 for(var i = 0; i < 101; i++){
-    if( i == 2) {
-    console.log("Even") }
-    else { "odd"}
+    
+    if( i %= 2) {    
+    console.log("Even") 
+    } 
+    else { 
+        console.log("odd");
+    } break;
     
 }
